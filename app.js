d1c466d91b1092c9fe2c7753b6fd845daadca63b
Menoen <791300479@qq.com>
2020-05-28 03:40:53 +0800

    module completed

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+//app.set('view engine', 'jade');更换成art-template
+app.engine('.html',require('express-art-template'));
+app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.url);
+  res.render('index', { 
+    title: 'HELLO',
+    name: 'Menoen',
+    age: '26',
+    happy: true
+ });
+});
+router.get('/world/:id', function(req,res,next) {
+  console.log('111111');
+  res.render('index', { title: 'HELLO WORLD1' });
 });
 
 module.exports = router;
